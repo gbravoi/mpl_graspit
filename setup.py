0cb4732e6c5ecd3db01b83c2842bbfc1f04d2e2f
gbravoi <glbravo@uc.cl>
2021-03-07 11:45:25 -0800

    change in maintainer email

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+This file is needed to transform mpl_graspit as a python package accesible by other modules
+chekc more info
+http://docs.ros.org/en/jade/api/catkin/html/user_guide/setup_dot_py.html
+"""
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
